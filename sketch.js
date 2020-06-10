07907605d5383874ef7db62ec4c8b543a5acf6e2
Avaneesh47 <63141175+Avaneesh47@users.noreply.github.com>
2020-06-10 20:00:51 +0530

    Uploaded the project files

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var canvas, backgroundImage;
+var athlete1,athlete2,athlete3,athlete4;
+var athletes;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form,player,game;
+
+function setup() {
+  canvas = createCanvas(displayWidth,displayHeight-200);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
